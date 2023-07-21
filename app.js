f3b08fbb83ab311818d10d8b3eb3fab09fb8dd4c
Tommytee1499 <tommytee1499@gmail.com>
2023-07-21 19:55:57 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const inputEmail = document.querySelector('#exampleInputEmail1')
+const password = document.querySelector('#exampleInputPassword1')
+const form = document.querySelector('#form')
+const myBtn =document.querySelector("#btn")
+const modal= document.getElementById('modal')
+const close = document.getElementById('close')
+
+
+myBtn.addEventListener('click',()=>{
+   modal.style.display = "block";
+})
+
+form.addEventListener('submit',(e)=>{
+e.preventDefault()
+
+validateInput()
+})
+
+
+const validateInput = ()=>{
+    inputValue = inputEmail.value.trim()
+    passwordValue = password.value.trim()
+
+    if(inputEmail.value=== '' && password.value === ''){
+        alert('email and password is required') 
+     } else {
+        window.location.href="dashboard.html"
+     }
+
+}
+
+
+
+
